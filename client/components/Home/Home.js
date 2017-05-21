6978b02a5c5ae2b5400fa939d604e321efe58ca8
Matt Trifilo <matt.trifilo@gmail.com>
2017-05-21 15:22:22 -0700

    build out home ui

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -4,8 +4,10 @@ import SearchInput from './SearchInput'
 
 const Home = props => {
   return (
-    <div className='Home-div'>
-      <h1 className='text-center page-title'>Bar Hop</h1>
+    <div className='Home_div'>
+      <h1 className='text-center Layout_page-title Home_page-title'>
+        Bar Hop
+      </h1>
       <SearchInput />
       <p><i className='fa fa-map-marker' /> Get current location</p>
     </div>
